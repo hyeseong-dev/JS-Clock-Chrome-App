41a3c53b1eed79b0b6f0cbbb7fb92507c2362cc2
이혜성 <hyeseong43@gmail.com>
2021-05-18 23:19:18 +0900

    [To Do List - 3] 1. 상수 toDos 배열을 일반변수 배열 타입으로 변경 2. saveToDos()함수안에 delBtn의 함수인 addEventListener함수를 이용하여    투두리스트를 클릭(x버튼)하면 삭제하는 기능이 발생되도록 첫번째는    이벤트의 이름인 문자열과 두번째 매개변수에는 호출할 함수를 넣습니다. 3. deleteToDo함수 정의부에서는 btn, li 상수를 정의하는데요. btn은 정의된    함수부의 매개변수인 event를 가져와서 target속성의 값을 할당받아요.    li는 btn상수의 parentNode속성을 가져와서 해당 button태그의 부모    태그인 li태그를 가져와요. 4. 전역변수toDoList가 deleteToDo함수 내에서 removeChild()함수를 사용하여    li 변수를 매개변수로 받아 삭제합니다. 5. 또한 cleanToDos를 상수로 정의하는데요. 이때 toDos 일반 Array 변수의    filter함수를 사용합니다. 내부에 함수를 정의해서 return시 좌항은    toDo.id로 객체의 id속성을 나타내고 우항은 parseInt(li.id)를 사용하여    문자타입인 숫자형태를 완벽히 정수형으로 변경하여 서로 비교하게    됩니다.

--- todo.js
+++ todo.js
@@ -4,7 +4,24 @@ const toDoForm = document.querySelector('.js-toDoForm'), // 여기서는 모듈�
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+function filterFn(toDo) { // 매개변수는 JSON객체를 지칭
+    return toDo.id === 1
+}
+
+let toDos = [];
+let idNumbers = 1;
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li= btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id != parseInt(li.id)
+    });
+    toDos = cleanToDos
+    saveToDos();
+}
+
 
 function saveToDos() { // localStorage에 toDos내용들을 저장함.
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,9 +31,10 @@ function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
-    const newId = toDos.length + 1;
-
+    const newId = idNumbers++;
+    
     delBtn.innerText = "❌";
+    delBtn.addEventListener('click', deleteToDo)
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
